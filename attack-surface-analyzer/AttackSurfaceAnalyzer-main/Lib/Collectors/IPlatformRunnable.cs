3df40afc53d32167e1414028dc423a5fd60c70ca
Ryan Restivo <53523282+ryanmrestivo@users.noreply.github.com>
2022-12-31 13:19:08 -0500

    asa update

--- attack-surface-analyzer/AttackSurfaceAnalyzer-main/Lib/Collectors/IPlatformRunnable.cs
+++
@@ -1,8 +0,0 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT License.
-namespace Microsoft.CST.AttackSurfaceAnalyzer.Collectors
-{
-    public interface IPlatformRunnable
-    {
-        bool CanRunOnPlatform();
-    }
-}
--- attack-surface-analyzer/AttackSurfaceAnalyzer-main/docfx/api/index.md
+++
@@ -1,3 +0,0 @@
-# Explore
-
-You can explore the Attack Surface Analyzer API using the navigation bar to the left.
