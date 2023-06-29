83ca95116096d3c653d3b75341b73314b2393a3b
ryanmrestivo <ryanmrestivo@gmail.com>
2023-06-28 21:00:40 -0400

    complete rewrite, added output

--- wifi.bat
+++ wifi.bat
@@ -1,66 +1,42 @@
-@echo off 
+@echo off
 mode 60,30
 setlocal enabledelayedexpansion
 
-:main 
-    title WiFiPasswordReveal 
+:: This script retrieves and displays the passwords of saved Wi-Fi networks on a Windows machine.
+:: It asks the user if they want to save the results to a file named 'wifi.txt'.
 
-    :: Get all the profiles
-    call :get-profiles r
+:: Store the output in a variable
+set "output="
+set "output=!output!WiFi Passwords:`n"
 
-    :: For each profile, try to get the password
-    :main-next-profile
-        for /f "tokens=1* delims=," %%a in ("%r%") do (
-            call :get-profile-key "%%a" key
-            if "!key!" NEQ "" (
-                echo WiFi: [%%a] Password: [!key!]
-            )
-            set r=%%b
-        )
-        if "%r%" NEQ "" goto main-next-profile
-
-    echo.
-    pause
-
-    goto :eof
-
-::
-:: Get the WiFi key of a given profile
-:get-profile-key <1=profile-name> <2=out-profile-key>
-    setlocal
-
-    set result=
-
-    FOR /F "usebackq tokens=2 delims=:" %%a in (
-        `netsh wlan show profile name^="%~1" key^=clear ^| findstr /C:"Key Content"`) DO (
-        set result=%%a
-        set result=!result:~1!
+:: Get all the profiles
+FOR /F "usebackq tokens=2 delims=:" %%a in (
+    `netsh wlan show profiles ^| findstr /C:"All User Profile"`) DO (
+    
+    :: Trim the profile name
+    set "profile=%%a"
+    set "profile=!profile:~1!"
+    
+    :: Get the WiFi key of the profile
+    FOR /F "usebackq tokens=2 delims=:" %%b in (
+        `netsh wlan show profile name^="!profile!" key^=clear ^| findstr /C:"Key Content"`) DO (
+        
+        :: Trim the key
+        set "key=%%b"
+        set "key=!key:~1!"
+        
+        :: Display the password in the terminal
+        echo WiFi: [!profile!] Password: [!key!]
+        
+        :: Append the password to the output variable
+        set "output=!output!WiFi: [!profile!] Password: [!key!]`n"
     )
-    (
-        endlocal
-        set %2=%result%
-    )
+)
 
-    goto :eof
-
-::
-:: Get all network profiles (comma separated) into the result result-variable
-:get-profiles <1=result-variable>
-    setlocal
-
-    set result=
-
-   
-    FOR /F "usebackq tokens=2 delims=:" %%a in (
-        `netsh wlan show profiles ^| findstr /C:"All User Profile"`) DO ( 
-        set val=%%a
-        set val=!val:~1!
-
-        set result=%!val!,!result!
-    )
-    (
-        endlocal
-        set %1=%result:~0,-1%
-    )
-
-    goto :eof
+:: Ask the user if they want to save the output to a text file
+echo.
+set /p "saveToFile=Do you want to save the output to a text file (Y/N)? "
+if /i "%saveToFile%" equ "Y" (
+    echo !output! | findstr /r /c:".*" > "wifi.txt"
+    echo Results exported to wifi.txt
+)
